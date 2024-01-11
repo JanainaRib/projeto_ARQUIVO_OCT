ec0537f14da6b02075422cc196ae34722c85af34
JanainaRib <145599221+JanainaRib@users.noreply.github.com>
2024-01-11 12:06:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-import selenium as sl 
-From selenium import webdriver
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
 
 #    variaveis    #
 
-user = ""
-password = ""
+user = "br05731472"
+password = "janaina"
+servico = Service(ChromeDriverManager().install())
 navegador  = webdriver.Chrome(service=servico)
 url_base = 'https://cashview.hdtdigital.com.br/CashView/Login.aspx '
-
-filter_data = input('Qual data você deseja ? ')
-
-response = requests.get(url_base + produto_nome)
-
-site = BeautifulSoup(response.text, 'html.parser')
-
-driver = webdriver.Chrome() 
+navegador.get(url_base)
 
 #      login        #
-
+time.sleep (5)
 navegador.find_element(By.ID,"UserName").send_keys(user)
+time.sleep (5)
 navegador.find_element(By.ID,"Password").send_keys(password)
+time.sleep (5)
 navegador.find_element(By.ID,"LoginButton").click()
-navegador.find_element(By.ID,"LoginButton").click()
-
+time.sleep (5)
 
 
 
